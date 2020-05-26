a002b7b988f82d38d76b5b3989d31702de2acb53
Luis Navarro <lanavarrogs@gmail.com>
2020-05-26 11:39:58 -0500

    Ya se completo la parte de registro

--- js/main.js
+++ js/main.js
@@ -22,7 +22,6 @@
         var botonRegistro = document.getElementById('btnRegistro');
         var listaProductos = document.getElementById('lista-productos');
         var sumaTotal = document.getElementById('suma-total');
-        console.log(listaProductos);
 
         //Extras
         var camisas = document.getElementById('camisa_evento');
@@ -30,7 +29,16 @@
 
 
         calcular.addEventListener('click',calcularMontos);
-        paseDia.addEventListener('blur',mostrarDias);
+
+        paseDia.addEventListener('change',mostrarDias);
+        paseDosDias.addEventListener('change',mostrarDias);
+        paseCompleto.addEventListener('change',mostrarDias);
+
+        nombre.addEventListener('blur',validacionCampos);
+        apellido.addEventListener('blur',validacionCampos);
+        email.addEventListener('blur',validacionCampos);
+        email.addEventListener('blur',validarMail);
+
 
         //Funcion que calcula y muestra el resumen de la compra y su total
         function calcularMontos(event){
@@ -38,16 +46,15 @@
             if(regalo.value === ''){
                 alert("Debes elegir un regalo");
             }else{
-                var boletosDia = parseInt(paseDia.value,10)||0,
+                let boletosDia = parseInt(paseDia.value,10)||0,
                      boletosDosDias = parseInt(paseDosDias.value,10)||0,
                      boletosCompletos = parseInt(paseCompleto.value,10)||0, 
                      cantidadCamisas = parseInt(camisas.value,10)||0,
                      cantidadEtiquetas = parseInt(etiquetas.value)||0;
 
-                var totalPagar = (boletosDia*30) + (boletosDosDias*45) + (boletosCompletos*50) + ((cantidadCamisas*10)*.93) + (cantidadEtiquetas*2);
-                console.log(totalPagar);
+                let totalPagar = (boletosDia*30) + (boletosDosDias*45) + (boletosCompletos*50) + ((cantidadCamisas*10)*.93) + (cantidadEtiquetas*2);
 
-                var listadoProductos = [];
+                let listadoProductos = [];
 
                 if(boletosDia >=1){
                     listadoProductos.push(`${boletosDia} Pases por dia`);
@@ -74,13 +81,67 @@
                 sumaTotal.innerHTML = `<p>$ ${totalPagar.toFixed(2)}</p>`;
                 
             }
+        }
 
-           function mostrarDias(){
-                
+        //Funcion que muestra dependiendo de los pases los talleres por dia
+        function mostrarDias(){
+            let boletosDia = parseInt(paseDia.value,10)||0,
+                boletosDosDias = parseInt(paseDosDias.value,10)||0,
+                boletosCompletos = parseInt(paseCompleto.value,10)||0;
+
+            let diasElegidos = [];
+
+            if(boletosDia === 0){
+                document.getElementById('viernes').style.display='none';
+            }
+            if(boletosCompletos === 0){
+                document.getElementById('viernes').style.display='none';
+                document.getElementById('sabado').style.display='none';
+                document.getElementById('domingo').style.display='none';
+            }
+            if(boletosDosDias === 0){
+                document.getElementById('viernes').style.display='none';
+                document.getElementById('sabado').style.display='none';
+            }
+            if( boletosDia>0){
+                diasElegidos.push('viernes');
+            }
+            if(boletosDosDias>0){
+                diasElegidos.push('viernes','sabado');
+            }
+            if(boletosCompletos>0){
+                diasElegidos.push('viernes','sabado','domingo');
+            }
+            for(let i=0; i<diasElegidos.length;i++){
+                document.getElementById(diasElegidos[i]).style.display='block';
             }
 
-
         }
+
+
+        function validacionCampos(){
+            if(this.value === ''){
+                errorDiv.style.display='block';
+                errorDiv.innerHTML = "Este campo es obligatorio";
+                this.style.border = '2px solid red';
+            }else{
+                errorDiv.style.display = 'none';
+                this.style.border = 'none';
+            }
+        }
+
+
+        function validarMail(){
+            if(this.value.indexOf("@") > -1){
+                errorDiv.style.display = 'none';
+                this.style.border = 'none';
+            }else{
+                errorDiv.style.display='block';
+                errorDiv.innerHTML = "El correo no es valido";
+                this.style.border = '2px solid red';
+            }
+        }
+
 
     });//Dom content Loaded
 })();
