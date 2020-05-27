533209fe29210879dc15d3cc2355bcb19268d0e5
Luis Navarro <lanavarrogs@gmail.com>
2020-05-27 11:32:50 -0500

    Se agrego el plugin de animacion de numeros, se agrego el plugin para la cuenta regresiva y se agrego el plugin para las letras

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@
     var regalo = document.getElementById('regalo');
 
     document.addEventListener('DOMContentLoaded',function(){
+        
+
+        //Codigo del mapa de Leaflet
+        var map = L.map('mapa').setView([20.653367,-103.391297], 17);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([20.653367,-103.391297]).addTo(map)
+            .bindPopup('Expo Guadalajara')
+            .openPopup();
+
        
         //Campos datos usuarios
         var nombre = document.getElementById('nombre');
@@ -145,3 +158,42 @@
 
     });//Dom content Loaded
 })();
+
+
+// Codigo JQuery
+$(function(){
+    
+    //Programa de conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click',function () {
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+
+        return false;
+    });
+
+    //Animaciones para los Numeros
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6},1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15},1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3},1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9},1500);
+
+
+    //Cuenta Regresiva
+    $('.cuenta-regresiva').countdown('2020/12/10 09:00:00',function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
+
+
+    //Lettering
+    $('.nombre-sitio').lettering();
+});
